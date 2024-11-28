bcc16fe474db64ae4e22c4a4c2e6fe7f38629f6d
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-11-28 11:04:01 +0900

    fix: json-server 배포 주소 추가

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/testResults",
+  baseURL: "https://grizzly-nifty-visitor.glitch.me/testResults",
 });
 
 export const apiMoneyful = axios.create({
